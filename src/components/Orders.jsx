ac02f5c5b41f07611761b53982f484451d3b7911
sahithikarnati1 <sahithi.karnati@slu.edu>
2024-12-09 00:20:39 +0000

    Lab 7 submission

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { BASE_URL } from '../config';
 
 const Orders = () => {
@@ -9,6 +9,18 @@ const Orders = () => {
    * 1. Create a `fetchOrders` function that retrieves all orders from the database
    * 2. Using the `useEffect` hook, update the existing `orders` state object when `fetchOrders` is complete
    **/ 
+  useEffect(() => {
+    fetchOrders();
+  }, [])
+
+  const fetchOrders = () => {
+    fetch('${Base_URL}/orders')
+    .then((res) => res.json())
+    .then((data) => {
+      setOrders(data);
+    })
+  }
+
 
 
   return (
